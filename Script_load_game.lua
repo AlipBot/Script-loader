5fde9d4b0be434074982fb38fcdba16800f87db1
Azex creator <80237144+Azex229@users.noreply.github.com>
2022-02-10 12:50:32 +0800

    Create Script_load_game.lua

---
+++ Script_load_game.lua
@@ -0,0 +1,9 @@
+-----Load Script Game------
+
+if game.PlaceId == 4913581664 then
+    ----Cart Ride Into Rdite!-----
+    loadstring(game:HttpGet(""))()
+    else
+        ------Global Menu------
+        loadstring(game:HttpGet(""))()
+end
